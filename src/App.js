e5a9053ed5e163a73ba033831e0e081d894d3fcb
Atikur Rahman <fulldevatikur@gmail.com>
2026-07-21 17:55:45 +0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const authRoutes = require("./routes/auth.routes");
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
 
-export default App;
+app.use("/api/auth", authRoutes);
+
+module.exports = app;
